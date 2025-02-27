c62b83b25c1024f207e2b76ca46a93923e847ec2
AbbeyMfss <Abbey_thum_wai_yu@students.edu.sg>
2025-02-27 08:35:58 +0000

    game

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,59 +1,102 @@
+import { Scene } from 'phaser';
+
 const WIDTH = 1024;
+
 const HEIGHT = 768;
 
-class BallGame extends Phaser.Scene {
-    constructor() {
-        super('Game');
-        
-        this.ball = null;
-        this.leftPaddle = null;
-        this.rightPaddle = null;
-        this.ballInMotion = false; // Flag to track ball movement
-    }
+export class Game extends Scene {
 
-    preload() {
-        this.load.image('background', 'assets/background.png');
-        this.load.image('ball', 'assets/ball.png');
-        this.load.image('paddle', 'assets/paddle.png');
-    }
+constructor() {
 
-    create() {
-        // Set background image
-        this.add.image(WIDTH / 2, HEIGHT / 2, 'background');
+super('Game');
 
-        // Create ball with physics properties
-        this.ball = this.physics.add.image(WIDTH / 2, HEIGHT / 2, 'ball').setScale(0.05);
-        this.ball.setCollideWorldBounds(true);
-        this.ball.setBounce(1, 1);
-        this.ball.setVelocity(200, 200);
+// Initialise necessary variables
 
-        // Create paddles as static physics objects
-        this.leftPaddle = this.physics.add.staticImage(50, HEIGHT / 2, 'paddle');
-        this.rightPaddle = this.physics.add.staticImage(WIDTH - 50, HEIGHT / 2, 'paddle');
+this.ball = null;
 
-        // Enable collision between ball and paddles
-        this.physics.add.collider(this.ball, this.leftPaddle);
-        this.physics.add.collider(this.ball, this.rightPaddle);
+this.leftPaddle = null;
 
-        // Listen for SPACE key to start ball movement
-        this.input.keyboard.on('keydown-SPACE', this.launchBall, this);
-        
-        // Setup keyboard controls
-        this.cursors = this.input.keyboard.createCursorKeys();
-        this.wasd = this.input.keyboard.addKeys({
-            up: Phaser.Input.Keyboard.KeyCodes.W,
-            down: Phaser.Input.Keyboard.KeyCodes.S
-        });
-    }
+this.rightPaddle = null;
 
-    update() {
-        // Game update logic (if needed)
-    }
+// Flag to determine if the ball is in motion
 
-    launchBall() {
-        if (!this.ballInMotion) {
-            this.ball.setVelocity(200, 200);
-            this.ballInMotion = true;
-        }
-    }
+this.ballInMotion = false;
+
+this.wasd = null;
+this.cursors = null;
+
+}
+
+preload() {
+
+// Load necessary assets from the assets directory
+
+this.load.image('background', 'assets/background.png');
+
+this.load.image('ball', 'assets/ball.png');
+
+this.load.image('paddle', 'assets/paddle.png');
+
+}
+
+create() {
+
+// Add background, ball, and paddles to the scene
+
+this.add.image(WIDTH / 2, HEIGHT / 2, 'background').setScale(0.8, 0.8);
+
+this.ball = this.physics.add.image(WIDTH / 2, HEIGHT / 2, 'ball').setScale(0.05, 0.05).refreshBody();
+
+this.ball.setCollideWorldBounds(true);
+
+this.ball.setBounce(1, 1);
+
+this.leftPaddle = this.add.image(50, 384, "paddle");
+
+this.rightPaddle = this.add.image(974, 384, "paddle");
+
+this.cursors = this.input.keyboard.createCursorKeys();
+this.wasd = this.input.keyboard.addKeys({
+up: Phaser.Input.Keyboard.KeyCodes.W,
+down: Phaser.Input.Keyboard.KeyCodes.S
+} );
+
+// Listen for "keyspace down" event, calling startBall function upon press
+
+this.input.keyboard.on('keydown-SPACE', this.startBall, this);
+
+}
+
+update() {
+//leftPaddle movement logic
+if (this.wasd.up.isDown && this.leftPaddle.y > 0) {
+this.leftPaddle.y -=5;
+} else if (this.wasd.down.isDown && this.leftPaddle.y < HEIGHT) {
+this.leftPaddle.y +=5;
+}
+// rightPaddle movement logic
+if(this.cursors.up.isDown && this.rightPaddle.y > 0 ){
+this.rightPaddle.y -=5;
+} else if (this.cursors.down.isDown && this.rightPaddle.y < HEIGHT) {
+this.rightPaddle.y +=5;
+}
+}
+
+startBall() {
+
+if (!this.ballInMotion) { // checks flag to determine if ball is NOT in motion
+
+let initialVelocityX = 300 * (Phaser.Math.Between(0, 1) ? 1 : -1); // sets to either 300 or -300
+
+let initialVelocityY = 300 * (Phaser.Math.Between(0, 1) ? 1 : -1); // sets to either 300 or -300
+
+this.ball.setVelocity(initialVelocityX, initialVelocityY); // sets ball to RANDOM velocity
+
+this.ballInMotion = true; // sets flag to ball is in motion
+
+}
+
+}
+
+
 }
